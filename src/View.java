3616590b36ea76db9de33a5daa7710899016632d
Chaitanya Varma <chaitany@thoughtworks.com>
2015-06-15 11:18:27 +0530

    Take input from user in main class

--- src/View.java
+++ src/View.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
+//Takes input from user
 public class View {
+    public static void main(String args[]){
+        String input1,input2, input3;
+        Scanner input = new Scanner(System.in);
+            input1 = input.nextLine();
+            input2 = input.nextLine();
+            input3 = input.nextLine();
+    }
 }
